a5f4a5fbf126036c5b24252b020f131cc61b137a
mrgloom <gloomyviking@gmail.com>
2016-05-06 13:11:00 +0300

    Create run_me.py

---
+++ run_me.py
@@ -0,0 +1,11 @@
+from download_datasets import *
+
+#1
+#url = 'http://vis-www.cs.umass.edu/lfw/lfw-bush.tgz'
+#filename = 'data/lfw-bush.tgz'
+
+#2
+url = 'http://vis-www.cs.umass.edu/lfw/lfw-funneled.tgz'
+filename = 'data/lfw-funneled.tgz'
+
+download_lfw_dataset(url, filename)
