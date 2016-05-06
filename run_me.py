c2be4549985478b2a9096010138907f425668b83
mrgloom <gloomyviking@gmail.com>
2016-05-06 17:10:59 +0300

    Update run_me.py

--- run_me.py
+++ run_me.py
@@ -1,5 +1,10 @@
 from download_datasets import *
 
+#lfw.tgz
+#lfwa.tar.gz
+#lfw-deepfunneled.tgz
+#lfw-funneled.tgz
+
 #1
 #url = 'http://vis-www.cs.umass.edu/lfw/lfw-bush.tgz'
 #filename = 'data/lfw-bush.tgz'
